33e1100a50fa0d140bf712557666f439652ee9b6
Anwar_Hossain <ranakrphone@gmail.com>
2026-06-20 17:37:36 +0600

    backend setup completed

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Battleship Game API is running successfully...");
+});
+
+export default app;
